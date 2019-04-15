8eb85e2952acd51b7f378db0276eaae3c90b902d
omobukki <42176174+omobukki@users.noreply.github.com>
2019-04-15 16:18:39 -0500

    mongo-mongooes

---
+++ public/app.js
@@ -0,0 +1,8 @@
+// Take everything in database and use is in Json
+$.getJSON("/articles", function(date){
+    //for each of one of the element
+    for (var i = 0; i < data.length; i++) {
+        // Display the apropos information on the page
+        $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    }
+});
